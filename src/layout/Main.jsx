d5f47d06df34c45460ffc7446df37957db1fd5fe
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-18 19:58:11 +0600

    Footer and login added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,15 @@
 // import React from 'react';
 
+import { Outlet } from "react-router-dom";
 import NavigationBar from "../pages/NavigationBar/NavigationBar";
+import Footer from "../pages/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
             <NavigationBar></NavigationBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login.jsx/Login";
 
 const router = createBrowserRouter([
     {
         path: "/",
         errorElement:<ErrorPage></ErrorPage>,
         element: <Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            }
+        ]
     },
 ]);
 export default router;
