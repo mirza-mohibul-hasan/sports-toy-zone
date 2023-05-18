5a36300133d7ccb2e2f614d79f5f56aeea33fecf
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-19 02:41:58 +0600

    sub category successduly added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 // import React from 'react';
 
+import SubCategory from "../SubCategory/SubCategory";
+
 const Home = () => {
     return (
         <div>
             <h1 className="text-center text-8xl">Banner Section</h1>
             <h1 className="text-center text-8xl">Gallery Section</h1>
-            <h1 className="text-center text-8xl">Shop By Category</h1>
+            <SubCategory></SubCategory>
             <h1 className="text-center text-8xl">Extra 1</h1>
             <h1 className="text-center text-8xl">Extra 2</h1>
         </div>
---
+++ src/pages/SubCategory/SubCategory.jsx
@@ -0,0 +1,50 @@
+// import React from 'react';
+
+import { useEffect, useState } from "react";
+
+const SubCategory = () => {
+    const [toys, setToys] = useState([]);
+    const [activeTab, setActiveTab] = useState("Football");
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/toyscategory/${activeTab}`)
+        .then(res => res.json())
+        .then(result=> setToys(result))
+    },[activeTab])
+
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+    };
+    return (
+        <div>
+            <h1>Sub Category is here</h1>
+            <div className="flex justify-center gap-2">
+
+                <button onClick={()=>handleTabClick('Football')} 
+                className={`${activeTab=="Football"?"bg-[#2196f3] text-white":"text-[#2196f3] border border-[#2196f3]"} py-1 px-2 mx-1 rounded hover:bg-gray-500 font-semibold md:my-0`}
+                >Football</button>
+
+                <button onClick={()=>handleTabClick('Cricket')} 
+                className={`${activeTab=="Cricket"?"bg-[#2196f3] text-white":"text-[#2196f3] border border-[#2196f3]"} py-1 px-2 mx-1 rounded hover:bg-gray-500 font-semibold md:my-0`}
+                >Cricket</button>
+
+                <button onClick={()=>handleTabClick('Boxing')} 
+                className={`${activeTab=="Boxing"?"bg-[#2196f3] text-white":"text-[#2196f3] border border-[#2196f3]"} py-1 px-2 mx-1 rounded hover:bg-gray-500 font-semibold md:my-0`}
+                >Boxing</button>
+            </div>
+            <div>
+                {
+                    toys.map(toy=><div key={toy._id}>
+                        <img className="w-44" src={toy.toyImg} alt="" />
+                        <h1>{toy.toyName}</h1>
+                        <p>${toy.price}</p>
+                        <p>{toy.rating}</p>
+                        <button>Details</button>
+                    </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default SubCategory;
