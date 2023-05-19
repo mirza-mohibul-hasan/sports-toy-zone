8c5ba76eb296e083771b82d8fa137ca8e1411155
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-19 11:57:41 +0600

    all toys loaded successfull

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,6 +1,10 @@
 // import React from 'react';
 
+import { useLoaderData } from "react-router-dom";
+
 const AllToys = () => {
+    const alltoys = useLoaderData()
+    console.log(alltoys)
     return (
         <div>
             <h1>Hello from All Toys</h1>
