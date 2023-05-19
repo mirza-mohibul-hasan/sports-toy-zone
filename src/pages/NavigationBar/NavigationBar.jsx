295ced2b705b0c1e793ffba9afeacb81c024f1aa
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-19 23:43:59 +0600

    responsive navbar added

--- src/pages/NavigationBar/NavigationBar.jsx
+++ src/pages/NavigationBar/NavigationBar.jsx
@@ -2,7 +2,8 @@
 import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
-
+import logo from '../../assets/locomotive.png'
+import { GoThreeBars } from "react-icons/go";
 const NavigationBar = () => {
     const { logOut, user } = useContext(AuthContext)
     const navItems = <>
@@ -22,51 +23,24 @@ const NavigationBar = () => {
     }
     return (
         <>
-            <nav className='mt-4 w-11/12 mx-auto mb-5 md:flex justify-between items-center text-center '>
-
-                <div>
-                    <h1 className='text-3xl font-bold'>Sports Toys</h1>
-                </div>
-                <div className='font-semibold grid grid-cols-3 md:grid-cols-5'>
-                    <NavLink to='/' className='font-bold mx-5 my-2 md:my-0 hover:text-[#2196f3]'>Home</NavLink>
-                    <NavLink to='/alltoys' className='font-bold mx-5 my-2 md:my-0 hover:text-[#2196f3]'>All Toys</NavLink>
-                    {
-                        user && <>
-                            <NavLink to='/mytoys' className='font-bold mx-5 my-2 md:my-0 hover:text-[#2196f3]'>My Toys</NavLink>
-                            <NavLink to='/addatoy' className='font-bold mx-5 my-2 md:my-0 hover:text-[#2196f3]'>Add A Toy</NavLink>
-                        </>
-                    }
-                    <NavLink to='/blogs' className='font-bold mx-5 my-2 md:my-0 hover:text-[#2196f3]'>Blogs</NavLink>
-                </div>
-                <div className='flex justify-center'>
-                    {/* <button className='bg-[#2196f3] py-1 px-2 mx-1 rounded hover:bg-gray-500 font-semibold md:my-0 text-white'>Log out</button>
-        <Link to='/login'><button className='bg-[#2196f3] py-1 px-2 mx-1 rounded hover:bg-gray-500 font-semibold md:my-0 text-white'>Log in</button></Link> */}
-                    {
-                        user && <button className="w-8 tooltip tooltip-left tooltip-info" data-tip={user?.displayName}>
-                            <img src={user?.photoURL} className='rounded-3xl' alt="" />
-                        </button>
-                    }
-                    {
-
-                        user ? <button onClick={handleLogOut} className='bg-[#2196f3] py-1 px-2 mx-1 rounded hover:bg-gray-500 font-semibold md:my-0 text-white'>Log out</button> :
-                            <Link to='/login'><button className='bg-[#2196f3] py-1 px-2 mx-1 rounded hover:bg-gray-500 font-semibold md:my-0 text-white'>Log in</button></Link>
-                    }
-                </div>
-
-            </nav>
+            <div className="bg-[#2196f3] flex justify-around items-center gap-5 mt-4 w-11/12 mx-auto mb-5 md:hidden rounded-lg py-2">
+                <img src={logo} className='w-12' alt="LOgo" />
+                <h1 className='text-3xl font-bold text-gray-800'>Sports Toys</h1>
+            </div>
             <div className="navbar bg-base-100 mt-4 w-11/12 mx-auto mb-5">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <img src="" alt="" />
+                            <GoThreeBars className="text-3xl"></GoThreeBars>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             {navItems}
                         </ul>
                     </div>
-                    <Link to='/' className="btn btn-ghost normal-case text-xl">
-                        <img className='h-full' alt="LOgo" />
-                    </Link>
+                    <div className="md:flex justify-center gap-4 items-center hidden ">
+                        <img src={logo} className='h-12' alt="LOgo" />
+                        <h1 className='text-3xl font-bold text-gray-700'>Sports Toys</h1>
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
