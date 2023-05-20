eb3e1dc60ad236831a9399c3429e282bf46308e7
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-21 02:16:36 +0600

    Toy Details issue Fixed

--- src/pages/Home/Banner.jsx/Banner.jsx
+++ src/pages/Home/Banner.jsx/Banner.jsx
@@ -1,10 +1,15 @@
 // import React from 'react';
-
+import Aos from "aos";
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 const Banner = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, [])
     return (
-        <div className="mt-4 w-11/12 mx-auto mb-5 rounded-xl hero min-h-screen" style={{ backgroundImage: `url("banner.webp")` }}>
+        <div className="mt-4 w-11/12 mx-auto mb-5 rounded-xl hero min-h-screen" style={{ backgroundImage: `url("banner.webp")` }} >
             <div className="hero-overlay bg-opacity-50 rounded-xl"></div>
-            <div className="hero-content text-center text-neutral-content">
+            <div className="hero-content text-center text-neutral-content" data-aos="flip-up">
                 <div className="max-w-md md:max-w-4xl">
                     <h1 className="mb-5 text-3xl md:text-5xl font-bold">Find Your <span className="text-[#07dcca]">Toys Here</span></h1>
                     <p className="mb-5">Our toy shop is a magical wonderland filled with endless delights for children of all ages. From cuddly stuffed animals to educational toys, building sets to dolls, action figures to board games, there is something for every childs imagination to explore. Our arts and crafts section sparks creativity, while puzzles challenge young minds. Come and discover the joy of play in our vibrant toy shop.</p>
--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -15,21 +15,21 @@ const ToyDetails = () => {
         sellerName,
         toyImg,
         toyName } = toy || {};
-        useTitle('Toy Details')
+    useTitle('Toy Details')
     return (
         <div className="w-8/12 mx-auto my-10">
-            <div className="card card-side bg-base-100 shadow-xl flex-col md:flex-row">
-                <img className="flex-1 max-h-72 rounded-xl" src={toyImg} alt="Movie" />
+            <div className="card card-side bg-base-100 shadow-xl flex-col md:flex-row items-center">
+                <img className="flex-1 max-h-72 rounded-xl m-3" src={toyImg} alt="Movie" />
                 <div className="card-body flex-1">
-                    <h2 className="card-title text-3xl font-semibold">Name: {toyName}</h2>
-                    <h2 className="text-lg font-semibold">Seller: {sellerName}</h2>
-                    <h2 className="text-lg font-semibold">Contsct: {sellerEmail}</h2>
-                    <div className="flex gap-5">
-                        <h2 className="text-md font-semibold">Price: ${price}</h2>
-                        <h2 className="flex items-center text-md font-semibold">Ratings: {rating} <AiFillStar></AiFillStar></h2>
+                    <h2 className="card-title text-3xl font-semibold"><span className="text-gray-500">Name:</span> {toyName}</h2>
+                    <h2 className="text-lg font-semibold"><span className="text-gray-500">Seller:</span>{sellerName}</h2>
+                    <h2 className="text-lg font-semibold"><span className="text-gray-500">Contact:</span> {sellerEmail}</h2>
+                    <div className="flex gap-2">
+                        <p className="text-lg font-semibold">Price: ${price}</p>
+                        <p className="flex items-center text-lg font-semibold">Ratings: {rating} <AiFillStar className="text-amber-500"></AiFillStar></p>
                     </div>
-                    <h2 className="text-m font-semibold">Available Quantity: {quantity}</h2>
-                    <h2><span className="font-semibold">Description:</span> {description}</h2>
+                    <h2 className="text-m font-semibold"><span className="text-gray-500">Available:</span> {quantity} Pieces</h2>
+                    <h2><span className="font-semibold"><span className="text-gray-500">Description:</span></span> {description}</h2>
                 </div>
             </div>
         </div>
